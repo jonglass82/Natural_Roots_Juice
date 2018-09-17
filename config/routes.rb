372f40a3293715e52cab753bbfbc50e03e327158
Jon Glass <jonglass82@gmail.com>
2018-09-17 08:40:03 -0500

    added prior html and css from old static site

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-get '/welcome', to: 'welcome#home'  
+get '/welcome', to: 'welcome#home' 
+    
+get 'welcome/home', to: 'welcome#home'
 
 get 'welcome/menu', to:'welcome#menu'
     
 get 'welcome/about', to:'welcome#about'
+    
+get 'welcome/menu', to:'welcome#menu'
     
 root 'welcome#home'    
     
