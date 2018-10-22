40c315e738b143e4ed679e796ed75c58b5e67e79
Jon Glass <jonglass82@gmail.com>
2018-10-22 16:44:25 -0500

    added menu items and preparing migration for database

--- db/migrate/20181019204308_create_orders.rb
+++ db/migrate/20181019204308_create_orders.rb
@@ -1,6 +1,19 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
+        t.string :phone_number
+        t.string :name_for_order
+        
+        t.string :green_goodness
+        t.string :indigo_mood
+        t.string :sweet_potato_pie
+        t.string :red_goddess
+        t.string :lime_coco_melon
+        t.string :electrolyte_balancer
+        
+        t.text :quantity
+        
+        
     end
   end
 end
